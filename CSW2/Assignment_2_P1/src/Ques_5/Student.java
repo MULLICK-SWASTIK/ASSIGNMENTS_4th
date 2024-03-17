71772e6d36e20acf7a653280b903ddbff4e79d8c
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-17 19:06:10 +0530

    New Question 5 folder

---
+++ CSW2/Assignment_2_P1/src/Ques_5/Student.java
@@ -0,0 +1,15 @@
+package Ques_5;
+
+public class Student {
+    String name;
+    int rn, mark;
+
+    public Student(String name, int rn, int mark) {
+        this.name = name;
+        this.rn = rn;
+        this.mark = mark;
+    }
+
+    
+
+}
