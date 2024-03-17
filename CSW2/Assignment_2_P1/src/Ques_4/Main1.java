3fe256eb2b4b79a5a79825aa9d4b515e02bc4b6a
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-17 18:57:38 +0530

    Code correction

--- CSW2/Assignment_2_P1/src/Ques_4/Main1.java
+++ CSW2/Assignment_2_P1/src/Ques_4/Main1.java
@@ -1,40 +1,46 @@
 package Ques_4;
+
 import java.util.Scanner;
+
 public class Main1 {
-    public static void main(String[]args){
-        Scanner k=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
         System.out.println("How many students to enter? ");
-        Student []students=new Student[k.nextInt()];
+        Student[] students = new Student[k.nextInt()];
         System.out.println("\nEnter student details: ");
-        String name,s_name;
-        int rn,s_rn, totalMark, s_totalMark;
-        for (int i=0;i<students.length;i++){
+        String name, s_name;
+        int rn, s_rn, totalMark, s_totalMark;
+        for (int i = 0; i < students.length; i++) {
             k.nextLine();
             System.out.print("\nEnter name: ");
-            name=k.nextLine();
+            name = k.nextLine();
             System.out.print("Enter roll number: ");
-            rn=k.nextInt();
+            rn = k.nextInt();
             System.out.print("Enter total marks: ");
-            totalMark=k.nextInt();
-            students[i]=new Student(name,rn,totalMark);
-            System.out.println("\nEnter next set of data.....");
+            totalMark = k.nextInt();
+            students[i] = new Student(name, rn, totalMark);
+            System.out.println("\nDetails updated......");
         }
-        System.out.print("Enter searching details: \nEnter name: ");
+
+        System.out.print("\nEnter searching details: \nEnter name: ");
         k.nextLine();
-        s_name=k.nextLine();
+        s_name = k.nextLine();
         System.out.print("Enter roll number: ");
-        s_rn=k.nextInt();
+        s_rn = k.nextInt();
         System.out.print("Enter total marks: ");
-        s_totalMark=k.nextInt();
-        Student s_student=new Student(s_name,s_rn,s_totalMark);
-        boolean isFound = false;
-        for (int i=0;i<students.length;i++){
-            if (students[i].equals(s_student)){
-                isFound=true;
+        s_totalMark = k.nextInt();
+        Student s_student = new Student(s_name, s_rn, s_totalMark);
+
+        int isFound = 0;
+        for (int i = 0; i < students.length; i++) {
+            isFound = students[i].compareTo(s_student);
+            System.out.println(isFound);
+            if (isFound == 1) {
                 break;
             }
         }
-        if (isFound)
+
+        if (isFound == 1)
             System.out.println("\nStudent found");
         else
             System.out.println("Student not found!");
--- CSW2/Assignment_2_P1/src/Ques_4/Student.java
+++ CSW2/Assignment_2_P1/src/Ques_4/Student.java
@@ -1,20 +1,24 @@
 package Ques_4;
-class Student implements Comparable<Student>{
+
+class Student implements Comparable<Student> {
     String name;
     int rn, totalMark;
 
-    public Student(String name, int rn, int totalMark){
+    public Student(String name, int rn, int totalMark) {
         this.name = name;
         this.rn = rn;
         this.totalMark = totalMark;
     }
 
-    public String toString(){
-        return "Name: "+this.name+"\tRoll no.: "+this.rn+"\tTotal marks: "+this.totalMark;
+    public String toString() {
+        return "Name: " + this.name + "\tRoll no.: " + this.rn + "\tTotal marks: " + this.totalMark;
     }
 
-    public int compareTo(Student s){
-        return (this.name==s.name && this.rn==s.rn && this.totalMark==s.totalMark)?0:1;
+    public int compareTo(Student s) {
+        if (this.name == s.name && this.rn == s.rn && this.totalMark == s.totalMark)
+            return 0;
+        else
+            return 1;
     }
 
 }
