7b1a8281dc01d5f47cd1198ace22e9b04e6c4b47
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-18 15:37:30 +0530

    Added Question 4 folder and files within it

---
+++ CSW2/Assignment_2_P2/src/Ques_4/Ques_4_driver.java
@@ -0,0 +1,31 @@
+package Ques_4;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Ques_4_driver {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        LinkedList<Student> studentList = new LinkedList<>();
+        String name;
+        int age, mark;
+        char choice;
+        while (true) {
+            System.out.print("\nEnter name: ");
+            name = k.nextLine();
+            System.out.print("Enter age: ");
+            age = k.nextInt();
+            System.out.print("Enter mark: ");
+            mark = k.nextInt();
+            studentList.add(new Student(name, age, mark));
+
+            System.out.print("Want to enter more student? (y/n)  ");
+            choice = k.next().charAt(0);
+            if (choice == 'n' || choice == 'N')
+                break;
+            System.out.println("Next entry.......");
+            k.nextLine();
+        }
+
+    }
+}
---
+++ CSW2/Assignment_2_P2/src/Ques_4/Student.java
@@ -0,0 +1,36 @@
+package Ques_4;
+
+class Student {
+    String name;
+    int age, mark;
+
+    public Student(String name, int age, int mark) {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getMark() {
+        return this.mark;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+}
