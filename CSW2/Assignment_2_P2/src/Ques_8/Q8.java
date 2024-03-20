d89c143c87069345009b75946ee9576db483af52
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-20 21:18:53 +0530

    Added Question 8 folder and files within it

---
+++ CSW2/Assignment_2_P2/src/Ques_8/Q8.java
@@ -0,0 +1,12 @@
+package Ques_8;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Q8 {
+    public static void main(String[] args) {
+        String str1 = "silent", str2 = "listen";
+        HashMap<Character, Integer> anag=new HashMap<>();
+        
+    }
+}
