94fca6326b9656c2d0a8abeea47ba4f5dfb08ee7
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-20 21:41:18 +0530

    Updated Question 8

--- CSW2/Assignment_2_P2/src/Ques_8/Q8.java
+++ CSW2/Assignment_2_P2/src/Ques_8/Q8.java
@@ -7,13 +7,26 @@ public class Q8 {
     public static void main(String[] args) {
         String str1 = "silent", str2 = "listen";
         HashMap<Character, Integer> anag = new HashMap<>();
-        for (int i = 0; i < str1.length(); i++) {
-            if (anag.containsKey(str1.charAt(i)))
-                anag.put(str1.charAt(i), anag.get(str1.charAt(i)) + 1);
-            else
-                anag.put(str1.charAt(i), 1);
-        }
+        if (str1.length() == str2.length()) {
+            for (int i = 0; i < str1.length(); i++) {
+                if (anag.containsKey(str1.charAt(i)))
+                    anag.put(str1.charAt(i), anag.get(str1.charAt(i)) + 1);
+                else
+                    anag.put(str1.charAt(i), 1);
+            }
+            // System.out.println(anag);
+            for (int i = 0; i < str2.length(); i++) {
+                if (anag.containsKey(str2.charAt(i)))
+                    anag.put(str2.charAt(i), anag.get(str2.charAt(i)) - 1);
+                else
+                    anag.put(str2.charAt(i), 1);
+            }
+            // System.out.println(anag);
+            // for (int i = 0; i < str2.length(); i++) {
 
-        System.out.println(anag);
+            // }
+        } else {
+            System.out.println("Given Strings are not anagrams");
+        }
     }
 }
