913cf90be93994de275f9fa6e90b11c1bed40e42
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-20 21:32:48 +0530

    Updated Question 8

--- CSW2/Assignment_2_P2/src/Ques_8/Q8.java
+++ CSW2/Assignment_2_P2/src/Ques_8/Q8.java
@@ -6,7 +6,14 @@ import java.util.HashMap;
 public class Q8 {
     public static void main(String[] args) {
         String str1 = "silent", str2 = "listen";
-        HashMap<Character, Integer> anag=new HashMap<>();
-        
+        HashMap<Character, Integer> anag = new HashMap<>();
+        for (int i = 0; i < str1.length(); i++) {
+            if (anag.containsKey(str1.charAt(i)))
+                anag.put(str1.charAt(i), anag.get(str1.charAt(i)) + 1);
+            else
+                anag.put(str1.charAt(i), 1);
+        }
+
+        System.out.println(anag);
     }
 }
