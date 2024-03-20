3cb5d475fc97215c72f201aaca91f60076110113
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-20 21:07:22 +0530

    Updated Question 5

--- CSW2/Assignment_2_P2/src/Ques_5/Library.java
+++ CSW2/Assignment_2_P2/src/Ques_5/Library.java
@@ -11,8 +11,27 @@ public class Library {
         books.put(2, new Book("Chanakya Niti", 1587, "XYZ", 10));
         System.out.println(books);
         System.out.print("\nEnter book name to search: ");
-        // k.nextLine();
-        String book_name=k.nextLine();
-        
+        String book_name = k.nextLine();
+        for (Book book : books.values()) {
+            if (book.book_name.equals(book_name)) {
+                System.out.println(book_name + " is present in the Library.");
+                break;
+            }
+        }
+        System.out.print("Enter a key to remove: ");
+        int ke = k.nextInt();
+        books.remove(ke);
+        System.out.println("After removal: \n" + books);
+        k.close();
     }
 }
+
+// OUTPUT
+// {1=Name: Sapiens ID: 5195 Author: Yuval Noah Harari Quantity: 20, 2=Name:
+// Chanakya Niti ID: 1587 Author: XYZ Quantity: 10}
+
+// Enter book name to search: Sapiens
+// Sapiens is present in the Library.
+// Enter a key to remove: 2
+// After removal:
+// {1=Name: Sapiens ID: 5195 Author: Yuval Noah Harari Quantity: 20}
