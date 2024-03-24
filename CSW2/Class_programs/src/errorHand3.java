cf39257046df71d24b787626cd805c463cf4d6e3
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-24 21:27:08 +0530

    Updated errorHand3.java

--- CSW2/Class_programs/src/errorHand3.java
+++ CSW2/Class_programs/src/errorHand3.java
@@ -1,15 +1,16 @@
-import java.util.*;
+// import java.util.*;
 
 public class errorHand3 {
     public static void main(String[]arfs){
-        Scanner k=new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String str=k.nextLine();
+        // Scanner k=new Scanner(System.in);
+        // System.out.println("Enter a string: ");
+        // String str=k.nextLine();
+        String str="My name is Swastik";
         String[] str_arr=str.split("");
         try{
             boolean isExist=false;
-            for (String s:str_arr){
-                if (s=="a"||s=="e"||s=="i"||s=="o"||s=="u"||s=="A"||s=="E"||s=="I"||s=="O"||s=="U"){
+            for (int i=0;i<str_arr.length;i++){
+                if( str_arr[i]=="a" || str_arr[i]=="e" || str_arr[i]=="i" || str_arr[i]=="o" || str_arr[i]=="u" || str_arr[i]=="A" || str_arr[i]=="E" || str_arr[i]=="I" || str_arr[i]=="O" || str_arr[i]=="U" ){
                     isExist=true;
                     break;
                 }
