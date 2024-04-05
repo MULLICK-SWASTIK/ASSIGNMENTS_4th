43aa52cc241f8949916ee410020d17f8ee64631d
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-04-06 02:01:19 +0530

    Updated OpenAddressing.java

--- SWASTIK_MULLICK_2241013379_AD2/Class_progs/src/LAB5/OpenAddressing.java
+++ SWASTIK_MULLICK_2241013379_AD2/Class_progs/src/LAB5/OpenAddressing.java
@@ -3,38 +3,171 @@ package LAB5;
 import java.util.*;
 
 public class OpenAddressing {
-    public static void main(String[] args) {
-        // Scanner k = new Scanner(System.in);
-        // System.out.println("Enter size: ");
-        // int n=k.nextInt(), arr[]=new int[n];
-        int ar[] = { 42, 19, 24, 0, 10 }, mod = ar.length, output[] = new int[mod];
+    int ar[];
+    static Scanner k = new Scanner(System.in);
+
+    void create() {
+        System.out.print("How many numbers to enter? ");
+        int mod = k.nextInt();
+        ar = new int[mod];
+        System.out.println("\nEnter elements: ");
         for (int i = 0; i < mod; i++) {
-            output[i] = Integer.MIN_VALUE;
+            ar[i] = Integer.MIN_VALUE;
         }
-        System.out.println("keys \t values");
         for (int j = 0; j < mod; j++) {
-            int i = 0, ind;
-            ind = (ar[j] + i) % mod;
-            while (output[ind] != Integer.MIN_VALUE) {
+            int inp = k.nextInt();
+            int i = 0, ind = (inp + i) % mod;
+            while (ar[ind] != Integer.MIN_VALUE) {
                 i++;
-                ind = (ar[j] + i) % mod;
+                ind = (inp + i) % mod;
             }
-            output[ind] = ar[j];
-            System.out.println(ind + " \t " + ar[j]);
+            ar[ind] = inp;
         }
     }
 
-    // int calcKey(int num, int output[]){
-    //     int i=0, ind;
-    //     ind=(num+i)%output.length;
-        
-    // }
-
-    void searchKey(int num){
-        boolean found=false;
-
+    void display() {
+        System.out.println("key \t values");
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] != Integer.MIN_VALUE)
+                System.out.println(String.format("%1$s \t %2$s", i, ar[i]));
+        }
     }
-    // int hashout(int val, int mod){
 
-    // }
+    void search() {
+        System.out.print("Enter number to search: ");
+        int inp = k.nextInt(), i = 0, ind = (inp + i) % ar.length;
+        while (inp != ar[ind] && i < ar.length) {
+            i++;
+            ind = (inp + i) % ar.length;
+        }
+        if (ar[ind] == inp) {
+            System.out.println(String.format("Found at key: %1$s", ind));
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    void delete() {
+        System.out.print("Enter element to delete: ");
+        int inp = k.nextInt(), i = 0, ind = (inp + i) % ar.length;
+        while (inp != ar[ind] && i < ar.length) {
+            i++;
+            ind = (inp + i) % ar.length;
+        }
+        if (ar[ind] == inp) {
+            System.out.println(String.format("Deleted element: %1$s", ar[ind]));
+            ar[ind] = Integer.MIN_VALUE;
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        OpenAddressing obj = new OpenAddressing();
+        while (true) {
+            System.out.println("\nSelect an option:-");
+            System.out.println("0: Exit");
+            System.out.println("1: Create new list");
+            System.out.println("2: Display list");
+            System.out.println("3: Search element");
+            System.out.println("4: Delete element");
+            System.out.print("\nEnter choice: ");
+            int choice = k.nextInt();
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                case 1:
+                    obj.create();
+                    break;
+                case 2:
+                    obj.display();
+                    break;
+                case 3:
+                    obj.search();
+                    break;
+                case 4:
+                    obj.delete();
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
+    }
+
 }
+// OUTPUT
+/*
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 1
+ * How many numbers to enter? 5
+ * 
+ * Enter elements:
+ * 42 19 24 5 10
+ * 
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 2
+ * key values
+ * 0 24
+ * 1 5
+ * 2 42
+ * 3 10
+ * 4 19
+ * 
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 3
+ * Enter number to search: 5
+ * Found at key: 1
+ * 
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 4
+ * Enter element to delete: 42
+ * Deleted element: 42
+ * 
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 2
+ * key values
+ * 0 24
+ * 1 5
+ * 3 10
+ * 4 19
+ * 
+ * Select an option:-
+ * 0: Exit
+ * 1: Create new list
+ * 2: Display list
+ * 3: Search element
+ * 4: Delete element
+ * 
+ * Enter choice: 0
+ */
