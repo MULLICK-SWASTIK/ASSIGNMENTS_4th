b9b760ce94ac960c57aa5522a3a1e182a3e1863c
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-05-26 23:36:29 +0530

    Updated assignment 9 question 3

--- CSW2/Assignment_9/src/Ques_3/MatMul.java
+++ CSW2/Assignment_9/src/Ques_3/MatMul.java
@@ -4,14 +4,62 @@ multiplying rows of the matrices among multiple threads to improve performance.
  */
 package Ques_3;
 
-import java.util.Arrays;
-
 public class MatMul {
-    public static void main(String[]args){
-        // System.out.println(Arrays.deep);
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        int[][] matrix2 = {
+                { 9, 8, 7 },
+                { 6, 5, 4 },
+                { 3, 2, 1 }
+        };
+        int[][] result = multiplyMatrices(matrix1, matrix2);
+        System.out.println("Result:");
+        printMatrix(result);
+    }
+
+    public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2) {
+        int m = matrix1.length, n = matrix2[0].length, p = matrix2.length;
+        int[][] result = new int[m][n];
+        Thread[] threads = new Thread[m];
+        for (int i = 0; i < m; i++) {
+            final int row = i;
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < p; k++) {
+                        result[row][j] += matrix1[row][k] * matrix2[k][j];
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
 /*
  * OUTPUT------------------------------------------
- * 
+ * Result:
+ * 30 24 18
+ * 84 69 54
+ * 138 114 90
  */
