b5faeebbe6780fcbae89df52eff773b16bbeba4d
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-05-26 16:53:56 +0530

    Modified remNode.java in assignment 7 question 7

--- CSW2/Assignment_7/src/Ques_7/remNode.java
+++ CSW2/Assignment_7/src/Ques_7/remNode.java
@@ -12,18 +12,6 @@ package Ques_7;
 import java.util.Scanner;
 import Ques_1_2.*;
 
-class TreeNode {
-    int info;
-    TreeNode left;
-    TreeNode right;
-
-    public TreeNode(int info) {
-        this.info = info;
-        this.left = null;
-        this.right = null;
-    }
-}
-
 public class remNode {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
